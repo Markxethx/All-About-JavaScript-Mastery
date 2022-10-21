12ad70b4633e10bbae9b858d415c0cc3932f74a3
The_Blank.eth <103093516+Markxethx@users.noreply.github.com>
2022-10-21 15:18:48 +0800

    Update index.js

--- project#19-MemePicker/index.js
+++ project#19-MemePicker/index.js
@@ -1,72 +1,102 @@
-import { catsData } from '/data.js'
+import {catsData} from './data.js';
 
-const emotionRadios = document.getElementById('emotion-radios')
-const getImageBtn = document.getElementById('get-image-btn')
-const gifsOnlyOption = document.getElementById('gifs-only-option')
+window.addEventListener('load', () => {
+    const emotionRadio = document.getElementById('emotion-radios');
+    const gifsOnlyOption = document.getElementById('gifs-only-option');
+    const memeModal = document.getElementById('meme-modal')
+    const memeModalInner = document.getElementById('meme-modal-inner');
+    const getImageBtn = document.getElementById('get-image-btn');
+    const memeModalCloseBtn = document.getElementById('meme-modal-close-btn');
+    const catsEmotionArray = [];
+    emotionRadio.addEventListener('change', getHighLighted);
+    getImageBtn.addEventListener('click', renderCats);
 
-emotionRadios.addEventListener('change', highlightCheckedOption)
+    memeModalCloseBtn.addEventListener('click', function() {
+        memeModal.style.display = 'none';
+    });
 
-getImageBtn.addEventListener('click', getMatchingCatsArray)
+    function getHighLighted(e) {
+        const radios = document.getElementsByClassName('radio');
 
-function highlightCheckedOption(e){
-    const radios = document.getElementsByClassName('radio')
-    for (let radio of radios){
-        radio.classList.remove('highlight')
-    }
-    document.getElementById(e.target.id).parentElement.classList.add('highlight')
-}
+        for (let radio of radios) {
+            radio.classList.remove('highlight')
+        }
+        document.getElementById(e.target.id).parentElement.classList.add('highlight')
+    };
 
+    function getMatchingArray() {
+        if (document.querySelector('input[type="radio"]:checked')){
+            const selectedEmotion = document.querySelector('input[type="radio"]:checked').value;
+            const isGif = gifsOnlyOption.checked;
 
-function getMatchingCatsArray(){     
-    if(document.querySelector('input[type="radio"]:checked')){
-        const selectedEmotion = document.querySelector('input[type="radio"]:checked').value
-        const isGif = gifsOnlyOption.checked
-/*
-Challenge:
-1. Use the .filter() and .includes() methods to get 
-   an array of cats which have the selected emotion
-   in their emotionTags array. 
-2. Store this array in a const and log it out to check
-   it's working. Think: what would be a good name for the
-   const?
-*/  
-    }  
-}
+            const matchingCatsArray = catsData.filter(function(cat){
 
-function getEmotionsArray(cats){
-    const emotionsArray = []    
-    for (let cat of cats){
-        for (let emotion of cat.emotionTags){
-            if (!emotionsArray.includes(emotion)){
-                emotionsArray.push(emotion)
-            }
+                if (isGif) {
+                    return cat.emotionTags.includes(selectedEmotion) && cat.isGif
+                } else {
+                    return cat.emotionTags.includes(selectedEmotion)
+                }
+            });
+            return matchingCatsArray
         }
     }
-    return emotionsArray
-}
 
+    function getSingleObject() {
+        const catsArray = getMatchingArray();
 
-function renderEmotionsRadios(cats){
-        
-    let radioItems = ``
-    const emotions = getEmotionsArray(cats)
-    for (let emotion of emotions){
-        radioItems += `
-        <div class="radio">
-            <label for="${emotion}">${emotion}</label>
-            <input
-            type="radio"
-            id="${emotion}"
-            value="${emotion}"
-            name="emotions"
-            >
-        </div>`
+        if (catsArray.length === 1) {
+            return catsArray[0]
+        } else {
+            const random = Math.floor(Math.random() * catsArray.length)
+            return catsArray[random]
+        }
     }
-    emotionRadios.innerHTML = radioItems
-}
 
-renderEmotionsRadios(catsData)
+    function renderCats() {
+        const catsObject = getSingleObject();
 
+        memeModalInner.innerHTML = `
+        <img 
+        class="cat-img"
+        src="/images/${catsObject.image}" 
+        alt="${catsObject.alt}"
+        >
+        `
+
+        memeModal.style.display = 'flex';
+    }
+    function getSelectedEmotion(cats) {
+        for (let cat of cats) {
+            for (let emotion of cat.emotionTags) {
+                if (!catsEmotionArray.includes(emotion)) {
+                    catsEmotionArray.push(emotion)
+                };
+            };
+        };
+        return catsEmotionArray
+    };
+
+    function renderEmotionToDom(cats) {
+        let radioItem = ``;
+        let emotions = getSelectedEmotion(cats);
+        for (let emotion of emotions) {
+            radioItem += `
+            <div class="radio">
+            <label for="${emotion}">${emotion}</label>
+            <input 
+            type="radio" 
+            value="${emotion}"  
+            id="${emotion}"
+            name="emotions">
+            </div>
+            `
+        };
+
+        emotionRadio.innerHTML = radioItem;
+
+    }
+    renderEmotionToDom(catsData);
+})
 
 
 
