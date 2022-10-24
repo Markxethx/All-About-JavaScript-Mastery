202f5807aa0835473cb1112d028aa8aaa3143129
The_Blank.eth <103093516+Markxethx@users.noreply.github.com>
2022-10-24 22:37:07 +0800

    Update pratice.js

--- pratice.js
+++ pratice.js
@@ -41,3 +41,17 @@ function lookUpProfile(name, prop) {
 }
 
 lookUpProfile("Akira", "likes");
+
+  
+  //New countdown recursion
+  
+  function countup(n) {
+  if (n < 1) {
+    return [];
+  } else {
+    const countArray = countup(n - 1);
+    countArray.push(n);
+    return countArray;
+  }
+}
+console.log(countup(5));
