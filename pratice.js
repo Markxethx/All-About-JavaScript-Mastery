6f7b7ded4a27a7d5c42203834553ec600f8b1630
The_Blank.eth <103093516+Markxethx@users.noreply.github.com>
2022-10-25 22:11:48 +0800

    Update pratice.js

--- pratice.js
+++ pratice.js
@@ -55,3 +55,10 @@ lookUpProfile("Akira", "likes");
   }
 }
 console.log(countup(5));
+
+  
+  var myConcat = function(arr1, arr2) {
+  return arr1.concat(arr2);
+};
+
+console.log(myConcat([1, 2], [3, 4, 5]));
