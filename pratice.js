80b768f57e835ec04522aecb88acd1248b03611e
The_Blank.eth <103093516+Markxethx@users.noreply.github.com>
2022-10-30 22:36:37 +0800

    Update pratice.js

--- pratice.js
+++ pratice.js
@@ -89,3 +89,32 @@ const highToday = LOCAL_FORECAST.today.high;
 
 // Only change code above this line
 just trying to be the one
+
+
+//sample react
+import React from "react"
+import ReactDOM from "react-dom"
+
+function Page() {
+    return (
+        <div>
+            <header>
+                <nav>
+                    <img src="./react-logo.png" width="40px" />
+                </nav>
+            </header>
+            <h1>Reasons I'm excited to learn React</h1>
+            <ol>
+                <li>It's a popular library, so I'll be 
+                able to fit in with the cool kids!</li>
+                <li>I'm more likely to get a job as a developer
+                if I know React</li>
+            </ol>
+            <footer>
+                <small>© 2021 Ziroll development. All rights reserved.</small>
+            </footer>
+        </div>
+    )
+}
+
+ReactDOM.render(<Page />, document.getElementById("root"))
