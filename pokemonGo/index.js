bc05ff807c729dba3ac216d141fc3af03a3ea5bf
The_Blank.eth <103093516+Markxethx@users.noreply.github.com>
2022-11-17 16:47:16 +0800

    Update index.js

--- pokemonGo/index.js
+++ pokemonGo/index.js
@@ -1,141 +1,73 @@
-fetch("https://api.pikaserve.xyz/pokemon/random")
-    .then(data => data.json())
-    .then(item => console.log(item))
-
-
-
-// const url = "https://api.pikaserve.xyz/pokemon/random";
-// JSON.parse(url)
-// for (let i= 0; i < 5; i++) {
+// for (let i= 0; i < 6; i++) {
 //     fetch("https://api.pikaserve.xyz/pokemon/random")
 //     .then(data => data.json())
 //     .then(item => {
-//         console.log(item.id)
+//         for (let obj of item) {
+//             const mother = document.getElementById("mother");
+//             //mother div having a baby
+//             const baby = document.createElement("div");
+//             baby.classList.add("image", "bg-gray-900", "rounded-lg", "flex", "items-center", "justify-center", "relative", "px-2", "flex-col");
+//             mother.appendChild(baby)
+//             baby.innerHTML = `
+//             <div class="absolute top-0 left-0 bg-slate-500 pl-3 pr-7 pb-1 rounded-br-[20px]"><h2 class="text-white text-lg font-extrabold">${item.name.english}</h2></div>
+//             <!--Hp-->
+//             <div class="absolute top-0 right-0 bg-violet-700 pl-3 pr-7 pb-1 rounded-tr-lg"><h2 class="text-white text-lg font-extrabold"><sub>hp</sub> ${item.base == undefined || false ? "N/A" : item.base.HP}</h2></div> 
+//             <div class="absolute opacity-60 top-60 left-3 bg-red-700 pl-3 pr-7 rounded-tr-lg rounded-br-[50px]"><h2 class="text-white text-lg font-extrabold text-center opacity-100"><h2>Ability</h2></div> 
+//             <div class="absolute top-56 left-20 pl-3 pr-7 rounded-tr-lg rounded-br-[50px]"><h2 class="text-red-200 italic text-lg font-extrabold text-stroke">${item.profile.ability[0][0]}</h2></div> 
+//             <div class="absolute top-64 text-ellipsis w-[300px] overflow-auto left-3"><h2 class="text-white text-sm font-extrabold text-stroke">${item.description}</h2></div> 
+//             <img class="mt-10 bg-gradient-to-r from-cyan-900 to-red-900 rounded-lg" src="${item.image.hires}" alt="Cute Pokemon">
+//             <div class="rounded-tr-lg rounded-br-[50px]"><h2 class="text-red-200 italic text-lg font-extrabold text-center">${item.type}</h2></div>
+//             `
+//         }
 //         //mother div
-//         const mother = document.getElementById("mother");
-//         //mother div having a baby
-//         const baby = document.createElement("div");
-//         baby.classList.add("image", "bg-gray-900", "rounded-lg", "flex", "items-center", "justify-center", "relative");
-//         mother.appendChild(baby)
-//         //name of the pokemon
-//         const name = document.createElement("div")
-//         name.classList.add()
-//         baby.innerHTML = `<img src="${item.image.hires}" class="w-full h-full" alt="Cute Pokemon" />`
+       
+//         console.log(item.base)
+//         console.log(item.profile.ability)
 //     });
 // }
 
-// loadPokemon();
-
-// fetch("https://pokeapi.co/api/v2/pokemon/")
-//     .then(data => data.json())
-//     .then(item => {console.log(item)
-//     });
-
-// console.log(randomToFinite.length)
-// const image = document.querySelectorAll(".image")
-// // const mother = document.getElementById("mother")
-// image.forEach(e => {
-//     fetch("https://api.pikaserve.xyz/pokemon/random")
-//     .then(data => data.json())
-//     .then(item => {
-//         e.innerHTML = `<img src="${item.image.hires}" class="w-full h-full" alt="Cute Pokemon" />`
-//     });
-// })
-
-
-// const image = document.querySelectorAll(".image")
-// image.forEach(e => {
-//     fetch("https://api.pikaserve.xyz/pokemon/random")
-//     .then(data => data.json())
-//     .then(item => {
-//         for (let i =0; i < item.length; i++) {
-//             randomToFinite.push()
-//             e.innerHTML = `<img src="${item.image.hires}" class="w-full h-full" alt="Cute Pokemon" />`
-//         }
-//     });
-// })
-
-
-// fetch("https://api.pikaserve.xyz/pokemon")
-//     .then(data => data.json())
-//     .then(item => console.log(item));
-
-
-// fetch("https://api.pikaserve.xyz/pokemon/random")
-//     .then(data => data.json())
-//     .then(item => console.log(item))
-
-
-
-// const url = "https://api.pikaserve.xyz/pokemon/random";
-// JSON.parse(url)
-for (let i= 0; i < 11; i++) {
+for (let i=0; i <4; i++) {
     fetch("https://api.pikaserve.xyz/pokemon/random")
     .then(data => data.json())
     .then(item => {
-        //mother div
-        const mother = document.getElementById("mother");
-        //mother div having a baby
-        const baby = document.createElement("div");
-        baby.classList.add("image", "bg-gray-900", "rounded-lg", "flex", "items-center", "justify-center", "relative");
-        mother.appendChild(baby)
-        //name of the pokemon
-        // const nameMother = document.createElement("div")
-        // nameMother.classList.add("absolute", "top-0", "left-0", "bg-slate-500", "pl-3", "pr-7", "pb-1", "rounded-br-[20px]")
-        // baby.appendChild(nameMother)
-        // const nameBaby = document.createElement("h2")
-        // nameBaby.classList.add("text-white", "text-lg", "font-extrabold")
-        // nameMother.appendChild(nameBaby)
-        // baby.appendChild(nameMother)
-        // console.log(item.name.english)
-        // nameMother.innerHTML = `<h2 class="text-white text-lg font-extrabold">${item.name.english}h</h2>`
-        baby.innerHTML = `
-        <div class="absolute top-0 left-0 bg-slate-500 pl-3 pr-7 pb-1 rounded-br-[20px]"><h2 class="text-white text-lg font-extrabold">${item.name.english}</h2></div>
-        <!--Hp-->
-        <div class="absolute top-0 right-0 bg-violet-700 pl-3 pr-7 pb-1 rounded-tr-lg"><h2 class="text-white text-lg font-extrabold"><sub>hp</sub> ${item.base.HP}</h2></div> 
-        <div class="absolute opacity-60 top-60 left-0 bg-red-700 pl-3 pr-7 rounded-tr-lg rounded-br-[50px]"><h2 class="text-white text-lg font-extrabold text-center opacity-100"><h2>Ability</h2></div> 
-        <div class="absolute top-56 left-20 pl-3 pr-7 rounded-tr-lg rounded-br-[50px]"><h2 class="text-red-200 italic text-lg font-extrabold text-center opacity-100">${item.profile.ability[0]}</h2></div> 
-        <div class="absolute top-64 text-ellipsis w-[300px] overflow-auto left-0"><h2 class="text-white text-base font-bold">${item.description}</h2></div> 
-        <div class="absolute top-96 left-20 pl-3 pr-7 rounded-tr-lg rounded-br-[50px]"><h2 class="text-red-200 italic text-lg font-extrabold text-center opacity-100">${item.type}</h2></div>
-        <img class="mt-16" src="${item.image.hires}" alt="Cute Pokemon">
-        `
-        console.log(item)
-        console.log(item.description)
-    });
+        for (let i=0; i<1; i++) {
+            let obj = []
+            obj.push(item)
+            for (let objs of obj) {
+                if (!obj.includes(objs.id)) {
+                    const mother = document.getElementById("mother");
+                    //mother div having a baby
+                    const baby = document.createElement("div");
+                    baby.classList.add("image", "bg-gray-900", "rounded-lg", "flex", "items-center", "justify-center", "relative", "px-2", "flex-col");
+                    mother.appendChild(baby)
+                    baby.innerHTML = `
+                    <div class="absolute top-0 left-0 bg-slate-500 pl-3 pr-7 pb-1 rounded-br-[20px]"><h2 class="text-white text-lg font-extrabold">${objs.name.english}</h2></div>
+                    <!--Hp-->
+                    <div class="absolute top-0 right-0 bg-violet-700 pl-3 pr-7 pb-1 rounded-tr-lg"><h2 class="text-white text-lg font-extrabold"><sub>hp</sub> ${objs.base == undefined || false ? "N/A" : objs.base.HP}</h2></div> 
+                    <div class="absolute opacity-70 top-56 left-3 bg-red-700 pl-3 pr-7 rounded-tr-lg rounded-br-[50px]"><h2 class="text-black text-xs font-extrabold text-center opacity-100">Ability</h2></div> 
+                    
+                    <!--Ability-->
+                    <div class="absolute top-56 left-16">
+                    <div class="flex justify-center w-60 h-12">
+                        <h2 class="text-red-200 italic text-lg font-extrabold text-center text-stroke">${objs.profile.ability[0][0]}</h2>
+                    </div>
+                    </div> 
+
+                    <!--Species-->
+                    <div class="absolute top-80 opacity-70 left-3 bg-red-700 pl-1 pr-2 rounded-tr-2xl rounded-br-[50px]"><h2 class="text-black text-xs font-extrabold text-center opacity-100">Species</h2></div> 
+                    <div class="absolute top-80 left-16">
+                    <div class="flex justify-center w-60 h-12">
+                        <h2 class="text-red-200 italic text-lg font-extrabold text-center text-stroke">${objs.species}</h2>
+                    </div>
+                    </div> 
+                    
+                    <div class="absolute top-64 w-[300px] overflow-auto left-3"><h2 class="text-white text-xs modernWay font-extrabold text-stroke-des">${objs.description}</h2></div> 
+                    <img class="mt-10 bg-gradient-to-r from-cyan-400 to-teal-400 rounded-lg" src="${objs.image.hires}" alt="Cute Pokemon">
+                    <div class="rounded-tr-lg rounded-br-[50px]"><h2 class="text-red-200 italic text-lg font-extrabold text-center">${objs.type}</h2></div>
+                    `
+                }
+                console.log(obj)
+            }
+        }
+})
 }
-
-// loadPokemon();
-
-// fetch("https://pokeapi.co/api/v2/pokemon/")
-//     .then(data => data.json())
-//     .then(item => {console.log(item)
-//     });
-
-// console.log(randomToFinite.length)
-// const image = document.querySelectorAll(".image")
-// // const mother = document.getElementById("mother")
-// image.forEach(e => {
-//     fetch("https://api.pikaserve.xyz/pokemon/random")
-//     .then(data => data.json())
-//     .then(item => {
-//         e.innerHTML = `<img src="${item.image.hires}" class="w-full h-full" alt="Cute Pokemon" />`
-//     });
-// })
-
-
-// const image = document.querySelectorAll(".image")
-// image.forEach(e => {
-//     fetch("https://api.pikaserve.xyz/pokemon/random")
-//     .then(data => data.json())
-//     .then(item => {
-//         for (let i =0; i < item.length; i++) {
-//             randomToFinite.push()
-//             e.innerHTML = `<img src="${item.image.hires}" class="w-full h-full" alt="Cute Pokemon" />`
-//         }
-//     });
-// })
-
-
-// fetch("https://api.pikaserve.xyz/pokemon")
-//     .then(data => data.json())
-//     .then(item => console.log(item));
