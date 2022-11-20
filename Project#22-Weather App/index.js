0638d0f70e79a11f7c911b840ea5872e4a757741
The_Blank.eth <103093516+Markxethx@users.noreply.github.com>
2022-11-20 22:43:11 +0800

    Update index.js

--- Project#22-Weather App/index.js
+++ Project#22-Weather App/index.js
@@ -1,58 +1,40 @@
-let weather = {
-    apiKey: "801c6a2d325c35c7fbcf8244bc0bff9b",
-    fetchWeather(city) {
-        fetch("https://api.openweathermap.org/data/2.5/weather?q="
-        + city
-        + "&units=metric&appid="
-        + this.apiKey
-        )
-        .then(res => res.json())
-        .then(data => this.displayWeather(data))
-    },
-    displayWeather(data) {
-    console.log(data)
+<!doctype html>
+<html>
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.1/css/all.min.css">
+  <link href="output.css" rel="stylesheet">
+</head>
+<body>
+  <div class="flex items-center justify-center height w-full box-border">
+    <!--Body-->
+    <div class="bg-transparent border rounded-3xl h-96 w-80 flex flex-col mx-2">
+        <!--Search Bar-->
+        <div class="h-8 max-full rounded-2xl m-2 bg-gray-50 flex justify-between items-center px-6">
+            <div class="font-semibold">
+              <form id="form" action="">
+                <input id="input" onkeydown="search(this)" rounded placeholder="Search Here" />
+              </form>
+            </div>
+        <div id="search" class="font-semibold"><i class="fa-solid fa-magnifying-glass"></i></div>
+        </div>
+        <!--City Here-->
+        <div class="px-6 flex items-center justify-between text-4xl mt-4 text-stroke-description">
+            <h1 id="name" class="font-bold ">Manila</h1>
+            <div><img src="" id="icon" alt="Clouds" class="w-20 h-auto" srcset=""></div>
+        </div>
+        <!--Temperature, humidity, wind speed, sunrise, sunset-->
+        <div class="px-6 text-stroke-description tracking-widest font-bold">
+            <div id="temp" class="text-7xl mt-4">53</div>
+            <div><h2 id="description" class="mt-4 text-stroke-description-back text-2xl">Cloudy</h2></div>
+            <div id="humidity" class="text-stroke-description-back">Humidity: 63</div>
+            <div id="speed" class="text-stroke-description-back">WindSpeed: 4.5 km/hr</div>
+            <sub class="pb-2" id="country"></sub>
+        </div>
 
-    const {name} = data;
-    const {country} = data.sys;
-    const {temp, humidity} = data.main;
-    const {speed} = data.wind;
-    const {icon, description} = data.weather[0];
-
-    document.getElementById("name").innerHTML = `<h1 class="text-stroke-description">${name}</h1>`;
-    document.getElementById("icon").innerHTML = ``;
-    document.getElementById("icon").src =
-      "https://openweathermap.org/img/wn/" + icon + ".png";
-    document.getElementById("country").innerHTML = country;
-    document.getElementById("description").innerHTML = description.toUpperCase();
-    document.getElementById("temp").innerHTML = temp + "&#8451";
-    document.getElementById("humidity").innerHTML = `<h2>Humidity: ${humidity} %</h2>`;
-    document.getElementById("speed").innerHTML = `<h2>Wind Speed: ${speed} km/hr</h2>`;
-    document.body.style.backgroundImage =
-      "url('https://source.unsplash.com/1600x900/?" + name + "')";
-    },
-    search() {
-        this.fetchWeather(input.value);
-        console.log(input.value)
-    },
-}
-const input = document.getElementById("input");
-const search = document.getElementById("search");
-search.addEventListener("click", function() {
-    weather.search();
-    input.value = ""
-})
-
-// document.querySelector("#input")
-//   .addEventListener('change', (e) => {
-//     weather.fetchWeather(e.currentTarget.value);
-// });
-
-
-input.addEventListener("keyup", function (event) {
-    console.log(event)
-    if (event.key == "Enter") {
-      weather.search();
-    }
-});
-weather.fetchWeather("manila");
-
+    </div>
+  </div>
+</body>
+<script src="index.js" defer></script>
+</html>
