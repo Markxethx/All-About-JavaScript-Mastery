f11245837f214423847dcbb134e7766177f09662
The_Blank.eth <103093516+Markxethx@users.noreply.github.com>
2022-11-26 14:35:28 +0800

    Update index.js

--- Project#25-Harry Potter Theme letter/index.js
+++ Project#25-Harry Potter Theme letter/index.js
@@ -1,3 +1,5 @@
+//here we create a name for our fonts, files are available at https://drive.google.com/drive/folders/1TcX55loYVIVAkr92H-ZfG3_cKLMbYapR?usp=sharing
+
 const logo = {"Logo One": "images/logo1.png",
 "Logo Two": "images/logo2.png",
 "Logo Three": "images/logo3.png"},
@@ -9,6 +11,8 @@ font = {
     "Harry Potter": "ParryHotter",
     "'Legendary Runes" : "'Legendary Runes Free Trial"
 },
+
+
 selectLogo = document.querySelector(".selectLogo"),
 selectFont = document.querySelector(".selectFont"),
 img = document.querySelector("img"),
@@ -16,42 +20,65 @@ output = document.querySelector(".output"),
 input = document.querySelector(".input"),
 saveBtn = document.getElementById("save")
 
+
+//options for select
 for (const logos in logo) {
-    let option = `<option value="${logo[logos]}">${logos}</option>`
+    let selected;
+    if (img.src == "") {
+        selected = "images/logo1.png"
+    }
+    let option = `<option value="${logo[logos]}" ${selected}>${logos}</option>`
     selectLogo.insertAdjacentHTML("beforeend", option)
 };
 for (const fonts in font) {
-    let options = `<option value="${font[fonts]}">${fonts}</option>`
+    let selected;
+    if (output.style.fontFamily == "") {
+        selected = "RomanceFatalPrima"
+    }
+    let options = `<option value="${font[fonts]}" ${selected}>${fonts}</option>`
     selectFont.insertAdjacentHTML("beforeend", options)
 };
+
+//event listener for selecting logo
 selectLogo.addEventListener("click", () => {
     img.src = selectLogo.value
 });
+
+//event listener for selecting fonts
 selectFont.addEventListener("click", () => {
     output.style.fontFamily = selectFont.value
 });
 
+//to get the "type" text to other text area, select it's value equal input value
 input.addEventListener("input", (e)=> {
     // e.target.value = output.value;
     output.value = e.target.value
 });
+
+//textarea (output) should be readonly
 output.setAttribute("readonly", "readonly")
 
-saveBtn.addEventListener("click", ()=>{
+//save to local storage
+saveBtn.addEventListener("click", ()=> {
     localStorage.setItem("myLetter", JSON.stringify(input.value))
     localStorage.setItem("myFont", JSON.stringify(selectFont.value))
     localStorage.setItem("myLogo", JSON.stringify(selectLogo.value))
     alert("Save in the Browser")
-    input.value = JSON.parse( localStorage.getItem("myLetter"))
 })
 
 const leadsFromLocalStorage = JSON.parse( localStorage.getItem("myLetter"))
 const leadsFromLocalStorageLogo = JSON.parse( localStorage.getItem("myLogo"))
 const leadsFromLocalStorageFont = JSON.parse( localStorage.getItem("myFont"))
 
-if (leadsFromLocalStorage) {
-    if (leadsFromLocalStorage == "") {
+// items in the local storage
+//to know wether the user save the letter or not
+if (leadsFromLocalStorage || leadsFromLocalStorageFont || leadsFromLocalStorageLogo) {
+    if (leadsFromLocalStorage == "" || leadsFromLocalStorageFont == "" || leadsFromLocalStorageLogo == "") {
         input.setAttribute("placeholder", "Enter Your Letter here")
+        img.src = leadsFromLocalStorageLogo
+        output.style.fontFamily = leadsFromLocalStorageFont
+        selectLogo.value = leadsFromLocalStorageLogo
+        selectFont.value = leadsFromLocalStorageFont
     } else {
         input.value = leadsFromLocalStorage
         output.value = leadsFromLocalStorage
