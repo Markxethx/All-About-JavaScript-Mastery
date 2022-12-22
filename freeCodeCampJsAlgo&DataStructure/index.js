2fd369e283b6b54afeeb4b634eafa34cf8292bf6
The_Blank.eth <103093516+Markxethx@users.noreply.github.com>
2022-12-22 15:21:57 +0800

    Update index.js

--- freeCodeCampJsAlgo&DataStructure/index.js
+++ freeCodeCampJsAlgo&DataStructure/index.js
@@ -28,3 +28,26 @@ The console would display the strings anonymous and newAuthor.
 Notice the syntax used to invoke the getter and setter. They do not even look like functions. Getters and setters are important because they hide internal implementation details.
 
 Note: It is convention to precede the name of a private variable with an underscore (_). However, the practice itself does not make a variable private.
+
+solution:
+
+// Only change code below this line
+class Thermostat {
+  constructor(fahrenheit) {
+    this.fahrenheit = fahrenheit;
+  }
+  
+  get temperature() {
+    return (5 / 9) * (this.fahrenheit - 32);
+  }
+  
+  set temperature(celsius) {
+    this.fahrenheit = (celsius * 9.0) / 5 + 32;
+  }
+}
+// Only change code above this line
+
+const thermos = new Thermostat(76); // Setting in Fahrenheit scale
+let temp = thermos.temperature; // 24.44 in Celsius
+thermos.temperature = 26;
+temp = thermos.temperature; // 26 in Celsius
