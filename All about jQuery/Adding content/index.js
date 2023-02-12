3fbe8c05bda33f1bf21fee7f9af31c18a5344c96
The_Blank.eth <103093516+Markxethx@users.noreply.github.com>
2023-02-12 20:47:51 +0800

    Update index.js

--- All about jQuery/Adding content/index.js
+++ All about jQuery/Adding content/index.js
@@ -9,3 +9,11 @@ $("tweet-section div").prepend(tweet)
 //before and after are almost the same with append and prepend
 $("tweet-section div p").before(tweet)
 $("tweet-section div p").after(tweet)
+
+//html and text
+//html means you are trying to change the whole content of your target, text means you just want to change the text inside of an element
+//note that even the element will be written in the text
+
+$("tweet-section div").html(tweet)
+//now if we gonna use text we need to clear the tweet variable with no element
+$("tweet-section div p").text(tweet)
