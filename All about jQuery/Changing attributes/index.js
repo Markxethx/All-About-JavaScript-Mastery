61f57fd3b20957565a818ae19a13cef6f0dc76de
The_Blank.eth <103093516+Markxethx@users.noreply.github.com>
2023-02-12 21:19:46 +0800

    Create index.js

---
+++ All about jQuery/Changing attributes/index.js
@@ -0,0 +1,7 @@
+//.removeAttr means removing the attribute in the element
+//.attr means adding attribute to the element
+
+$("section img").removeAttr("alt");
+$("section img).attr("alt", "location");
+  
+//attr is powerfull also if you want to change the current source, like these image, just change the attr and change the source
