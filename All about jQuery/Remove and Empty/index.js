2b79184bd4456889098d0ac5456b99c2ef4cc2cc
The_Blank.eth <103093516+Markxethx@users.noreply.github.com>
2023-02-12 21:13:45 +0800

    Create index.js

---
+++ All about jQuery/Remove and Empty/index.js
@@ -0,0 +1,5 @@
+//.empty() just erace the content in the element, but tht elemen itself are still there
+//remove() is removing all the child or the target element
+
+$("section").empty();
+$("section img").remove();
