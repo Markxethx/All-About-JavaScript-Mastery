d30b54cf59da8ae2cc17cf7c2564f3d6b39958bc
The_Blank.eth <103093516+Markxethx@users.noreply.github.com>
2023-02-12 20:16:19 +0800

    Update index.js

--- All about jQuery/Adding content/index.js
+++ All about jQuery/Adding content/index.js
@@ -5,3 +5,7 @@ $("tweet-section div").append(tweet)
 
 //prefpnd is the reverse of the append, element will be added before the target elemen
 $("tweet-section div").prepend(tweet)
+
+//before and after are almost the same with append and prepend
+$("tweet-section div p").before(tweet)
+$("tweet-section div p").after(tweet)
