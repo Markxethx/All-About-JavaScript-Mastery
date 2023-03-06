8a9aaef1bc6b14d8a48424875e2a2a2be5ec7748
The_Blank.eth <103093516+Markxethx@users.noreply.github.com>
2023-03-06 22:44:15 +0800

    Update index.js

--- Regular Expression | FreeCodeCamp/Positive and negative look ahead/index.js
+++ Regular Expression | FreeCodeCamp/Positive and negative look ahead/index.js
@@ -1,10 +1,15 @@
-Lookaheads are patterns that tell JavaScript to look-ahead in your string to check for patterns further along. This can be useful when you want to search for multiple patterns over the same string.
+Lookaheads are patterns that tell JavaScript to look-ahead in your string to check for patterns further along. 
+This can be useful when you want to search for multiple patterns over the same string.
 
 There are two kinds of lookaheads: positive lookahead and negative lookahead.
 
-A positive lookahead will look to make sure the element in the search pattern is there, but won't actually match it. A positive lookahead is used as (?=...) where the ... is the required part that is not matched.
+A positive lookahead will look to make sure the element in the search pattern is there,
+  but won't actually match it. A positive lookahead is used as (?=...) where the ... is the required part that is not matched.
 
-On the other hand, a negative lookahead will look to make sure the element in the search pattern is not there. A negative lookahead is used as (?!...) where the ... is the pattern that you do not want to be there. The rest of the pattern is returned if the negative lookahead part is not present.
+On the other hand, a negative lookahead will look to make sure the element
+in the search pattern is not there. A negative lookahead is used as (?!...) 
+                                                                     
+where the ... is the pattern that you do not want to be there. The rest of the pattern is returned if the negative lookahead part is not present.
 
 Lookaheads are a bit confusing but some examples will help.
 
@@ -22,3 +27,8 @@ let password = "abc123";
 let checkPass = /(?=\w{3,6})(?=\D*\d)/;
 checkPass.test(password);
 Use lookaheads in the pwRegex to match passwords that are greater than 5 characters long, and have two consecutive digits.
+
+//solutions
+let sampleWord = "astronaut";
+let pwRegex =  /(?=\w{6})(?=\w*\d{2})/;
+let result = pwRegex.test(sampleWord);
